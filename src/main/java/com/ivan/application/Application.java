4b97e3f901042e299ca820e371ecac46258e4005
ivan-yar <ivanmc-007@mail.ru>
2020-04-15 18:17:16 +0300

    Nothing

--- src/main/java/com/ivan/application/Application.java
+++ src/main/java/com/ivan/application/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String ... args) {
         ApiContextInitializer.init();
         SpringApplication.run(Application.class, args);
+
     }
 
 }
