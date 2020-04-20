83b715cf4c70328fa7598c8e98435cce958b66e2
ivan-yar <ivanmc-007@mail.ru>
2020-04-20 15:32:45 +0300

    Clear target folder for repository

--- src/main/java/com/ivan/application/Application.java
+++ src/main/java/com/ivan/application/Application.java
@@ -10,7 +10,6 @@ public class Application {
     public static void main(String ... args) {
         ApiContextInitializer.init();
         SpringApplication.run(Application.class, args);
-
     }
 
 }
